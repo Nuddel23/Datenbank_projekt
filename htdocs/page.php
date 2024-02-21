3aa55e2abcd1a873ff9e38efc45e7776df57b4c9
Nuddel23 <john.schingarjow@web.de>
2024-02-21 17:09:49 +0100

    Benutzer mit student verknüpft

--- htdocs/page.php
+++ htdocs/page.php
@@ -6,17 +6,28 @@
         <h1><Center>Hallo
         <?php
             session_start();
-            $row=$_SESSION['row'];
-            if ($row["Roll_ID"] == "3"){
-                echo ("Admin"."</Center></h1>");
-            }
-            else{
-                echo ($row['Benutzername']."</Center></h1>");
-            }
-            foreach($row as $item){
-                echo ($item."</br>");
-            }   
+            $session_row=$_SESSION['row'];
+
+            $db = new mysqli('localhost', 'root', '', 'uni');              
+            $query = 
+                "SELECT `student`.*, `benutzer`.*
+                FROM `student` 
+                LEFT JOIN `benutzer` ON `student`.`ben_ID` = `benutzer`.`ID`;
+                ";
+            $result = $db->execute_query($query);
             
+            foreach($result as $row){
+                if ($row['ID'] == $session_row['ID']){
+                    $_SESSION['row']=$row;
+                    
+                    if ($row['Roll_ID'] == "3"){
+                        echo ("Admin"."</Center></h1>");
+                    }
+                    else{
+                        echo ($row['Name']."</Center></h1>");
+                    }   
+                }
+            }
         ?>    
     </body>
 </html>
