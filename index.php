5446711d35475107d2cb030f2d1c1909e602ceb9
Alex <a.piorek@web.de>
2024-05-07 12:40:20 +0200

    login page weter verbessered

--- index.php
+++ index.php
@@ -1,54 +1,50 @@
 <html>
-
-<head>
-    <title> Login Page </title>
-    <link rel="stylesheet" href="css/login.css?v=<?php echo time(); ?>">
-</head>
-
-<body>
-    <div class="login_fenster">
+    <head>
+        <title> Login Page </title>
+        <link rel="stylesheet" href="css/login.css?v=<?php echo time(); ?>">
+    </head>
+    <body>
+        <div class = "login_fenster" >
         <h1>Login</h1>
         <p></p>
         <form method="post" action="">
-            <input type="text" placeholder="Benutzername" name="Benutzername" /></br>
-            <input type="password" placeholder="Passwort" name="Passwort" /></br>
-            <input type="submit" name="submit" value="Login" />
+            <input type="text" placeholder="Benutzername" name="Benutzername"/></br>
+            <input type="password" placeholder="Passwort" name="Passwort"/></br>
+            <input type="submit" name="submit" value="Login"/>
         </form>
-    </div>
-    <?php
-    session_start();
-    if (isset($_SESSION["login"]) == false) {
-        $_SESSION["login"] = false;
-    }
-    // if ($_SESSION['login'] == true){
-    //     header("Location: homepage.php");
-    // }
-    
-    if (isset($_POST["submit"])) {
-        require $_SERVER['DOCUMENT_ROOT'] . "/Datenbank.php";
+        </div>
+        <?php
+            session_start();
+            if (isset($_SESSION["login"]) == false){
+                $_SESSION["login"] = false;
+            }
+            // if ($_SESSION['login'] == true){
+            //     header("Location: homepage.php");
+            // }
 
-        $query = "SELECT `benutzer`.*, `benutzer_id`.*
+            if (isset($_POST["submit"])){
+                require $_SERVER['DOCUMENT_ROOT']."/Datenbank.php";   
+                
+                $query = "SELECT `benutzer`.*, `benutzer_id`.*
                             FROM `benutzer` 
                             LEFT JOIN `benutzer_id` ON `benutzer`.`ID` = `benutzer_id`.`Ben_ID`;";
-        $result = $db->execute_query($query);
-        $name = $_POST["Benutzername"];
-
-        foreach ($result as $row) {
-            if ($row["Benutzername"] == $name) {
-                if (hash_equals(hash_hmac('sha256', $_POST["Passwort"], $name), $row["Passwort"])) { //Passwort entschlüsseln
-                    $_SESSION['login'] = true;
-                    $_SESSION["benutzer"] = $row;
-                    $_SESSION["Roll_ID"] = $row["Roll_ID"];
-                    header("Location: homepage.php");
-                    exit;
-                }
+                $result = $db->execute_query($query);
+                $name = $_POST["Benutzername"];  
+                foreach ($result as $row) {
+                    if ($row["Benutzername"] == $name) {
+                        if (hash_equals(hash_hmac('sha256', $_POST["Passwort"], $name), $row["Passwort"])) { //Passwort entschlüsseln
+                            $_SESSION['login']=true;
+                            $_SESSION["benutzer"]=$row;
+                            $_SESSION["Roll_ID"]=$row["Roll_ID"];
+                            header("Location: homepage.php");
+                            exit;
+                        }
+                    }
+                } 
+                echo nl2br ("Login fehlgeschlagen\rPasswort oder Benutzername ist falsch");
+                exit;
             }
-        }
-        echo nl2br("Login fehlgeschlagen\rPasswort oder Benutzername ist falsch");
-        exit;
-    }
-
-    ?>
-</body>
-
+        
+        ?>
+    </body>
 </html>
